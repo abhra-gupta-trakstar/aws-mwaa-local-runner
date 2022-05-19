d679f389cf66b1f0324f9a6aafa2b8a053057123
Abhra Gupta <abhra.gupta@trakstar.com>
2022-05-19 15:21:48 +0530

    Partitions DAG

--- dags/learn-check-partitions.py
+++ dags/learn-check-partitions.py
@@ -10,15 +10,17 @@ import boto3
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.email import EmailOperator
-# from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
+from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
 from airflow.utils.trigger_rule import TriggerRule
-# env_name = Variable.get("deploy_environment")
+from common.insights_lib import send_slack_message, send_email_message
 
-boto3.setup_default_session(profile_name='biqamfa')
+env_name = Variable.get("deploy_environment")
+dag_name = 'TS-Learn-Check-Partitions-Daily'
+email_recipients_list = ['abhra.gupta@trakstar.com']
 s3 = boto3.resource('s3')
-# env_name = Variable.get("deploy_environment")
+env_name = Variable.get("deploy_environment")
 
-S3_BUCKET_NAME = "applied-data-lake"
+S3_BUCKET_NAME = Variable.get("data_lake_bucket")
 EXEC_DATE = '{{ macros.ds_add(ds, 1) }}'
 EXEC_YEAR = '{{ macros.ds_format(macros.ds_add(ds, 1), "%Y-%m-%d", "%Y") }}'
 EXEC_MONTH = '{{ macros.ds_format(macros.ds_add(ds, 1), "%Y-%m-%d", "%m") }}'
@@ -37,13 +39,13 @@ default_args = {
     'retries': 0,
     'retry_delay': timedelta(minutes=2),
     'provide_context': True,
-    'email': ['abhra.gupta@trakstar.com', 'brian.kasen@trakstar.com', 'ritika.naidu@trakstar.com'],
+    'email': email_recipients_list,
     'email_on_failure': False,
     'email_on_retry': False
 }
 
 dag = DAG(  
-    'TS-Learn-Check-Partitions-Daily',
+    dag_name,
     default_args=default_args,
     dagrun_timeout=timedelta(hours=2),
     schedule_interval='0 11 * * *'
@@ -63,6 +65,7 @@ def folder_exists_and_not_empty(bucket, path) -> bool:
     Folder should exists. 
     Folder should not be empty.
     '''
+    boto3.setup_default_session(profile_name='biqamfa')
     s3 = boto3.client('s3')
     if not path.endswith('/'):
         path = path+'/' 
@@ -79,64 +82,58 @@ account_table = ['account', 'accountmetadata', 'accountrelation',
 course_table = ['cmodule', 'course', 'coursestate', 'cmodulestate' ,
            'coursestatehistory', 'mtype', 'coursecmodule']
 
-def _get_message(action, table_list) -> str:
-    MT = pytz.timezone('America/Denver')
-    mt_time = datetime.now(MT)
-    return "TS-Learn-Check-Partitions-Daily DAG {} for {} on {} Mountain Time".format(action, table_list, mt_time)
+send_slack_success_notification_account_table = SlackWebhookOperator(
+    task_id="send_slack_success_notification_account_table",
+    trigger_rule='all_success',
+    http_conn_id="slack_conn",
+    message=send_slack_message(dag_name, "completed successfully", 'America/Denver', account_table),
+    channel="#airflow-monitoring-{}".format(env_name),
+    dag=dag
+)
 
+send_slack_success_notification_course_table = SlackWebhookOperator(
+    task_id="send_slack_success_notification_course_table",
+    trigger_rule='all_success',
+    http_conn_id="slack_conn",
+    message=send_slack_message(dag_name, "completed successfully", 'America/Denver', course_table),
+    channel="#airflow-monitoring-{}".format(env_name),
+    dag=dag
+)
 
-# send_slack_success_notification_account_table = SlackWebhookOperator(
-#     task_id="send_slack_success_notification_account_table",
-#     trigger_rule='all_success',
-#     http_conn_id="slack_conn",
-#     message=_get_message("completed successfully", "account_table"),
-#     channel="#airflow-monitoring-{}".format(env_name),
-#     dag=dag
-# )
+send_slack_failure_notification_account_table = SlackWebhookOperator(
+            task_id="send_slack_failure_notification_account_table",
+            trigger_rule='one_failed',
+            http_conn_id="slack_conn",
+            message=send_slack_message(dag_name, "failed", 'America/Denver', account_table),
+            channel="#airflow-monitoring-{}".format(env_name),
+            dag=dag
+        )
 
-# send_slack_success_notification_course_table = SlackWebhookOperator(
-#     task_id="send_slack_success_notification_course_table",
-#     trigger_rule='all_success',
-#     http_conn_id="slack_conn",
-#     message=_get_message("completed successfully", "course_table"),
-#     channel="#airflow-monitoring-{}".format(env_name),
-#     dag=dag
-# )
+send_slack_failure_notification_course_table = SlackWebhookOperator(
+            task_id="send_slack_failure_notification_course_table",
+            trigger_rule='one_failed',
+            http_conn_id="slack_conn",
+            message=send_slack_message(dag_name, "failed", 'America/Denver', course_table),
+            channel="#airflow-monitoring-{}".format(env_name),
+            dag=dag
+        )
 
-# send_slack_failure_notification_account_table = SlackWebhookOperator(
-#             task_id="send_slack_failure_notification_account_table",
-#             trigger_rule='one_failed',
-#             http_conn_id="slack_conn",
-#             message=_get_message("failed", "account_table"),
-#             channel="#airflow-monitoring-{}".format(env_name),
-#             dag=dag
-#         )
-
-# send_slack_failure_notification_course_table = SlackWebhookOperator(
-#             task_id="send_slack_failure_notification_course_table",
-#             trigger_rule='one_failed',
-#             http_conn_id="slack_conn",
-#             message=_get_message("failed", "course_table"),
-#             channel="#airflow-monitoring-{}".format(env_name),
-#             dag=dag
-#         )
-
-# send_email_notification_account_table = EmailOperator(
-#         task_id="send_Email_account_table",
-#         trigger_rule='one_success',
-#         to=["abhra.gupta@trakstar.com", "brian.kasen@trakstar.com"],
-#         subject='TS-Learn-Check-Partitions-Daily-{}'.format(env_name),
-#         html_content="TS-Learn-Check-Partitions-Daily DAG has failed on account_table in {} environment".format(env_name),
-#         dag=dag
-#     ) 
-# send_email_notification_course_table = EmailOperator(
-#         task_id="send_Email_course_table",
-#         trigger_rule='one_success',
-#         to=["abhra.gupta@trakstar.com", "brian.kasen@trakstar.com"],
-#         subject='TS-Learn-Check-Partitions-Daily-{}'.format(env_name),
-#         html_content="TS-Learn-Check-Partitions-Daily DAG has failed on course_table in {} environment".format(env_name),
-#         dag=dag
-#     ) 
+send_email_notification_account_table = EmailOperator(
+        task_id="send_Email_account_table",
+        trigger_rule='one_success',
+        to= email_recipients_list,
+        subject='{}-{}'.format(dag_name, env_name),
+        html_content=send_email_message(account_table),
+        dag=dag
+    ) 
+send_email_notification_course_table = EmailOperator(
+        task_id="send_Email_course_table",
+        trigger_rule='one_success',
+        to=email_recipients_list,
+        subject='{}-{}'.format(dag_name, env_name),
+        html_content=send_email_message(course_table),
+        dag=dag
+    ) 
 
 
 for table in account_table:
@@ -151,3 +148,9 @@ for table in course_table:
     full_path = path_prefix + "{}/ingest_year={}/ingest_month={}/ingest_day={}/".format(table, current_year, current_month, current_day)
     python_task  = PythonOperator(task_id='find_partition_{}'.format(table), python_callable=folder_exists_and_not_empty, op_args=[S3_BUCKET_NAME, full_path ], dag=dag)
     course_table_partition_check_begin >> python_task >> course_table_partition_check_end
+
+
+account_table_partition_check_end >> send_slack_success_notification_account_table >> send_slack_failure_notification_account_table >> send_email_notification_account_table
+course_table_partition_check_end >> send_slack_success_notification_course_table >> send_slack_failure_notification_course_table >> send_email_notification_course_table
+
+
